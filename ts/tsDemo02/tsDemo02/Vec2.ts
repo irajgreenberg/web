7371cb7c6776ff17cf0a0a7d9efcbf95648c73a5
irajgreenberg <irajgreenberg@gmail.com>
2015-09-21 11:23:17 -0500

    adding html5 examples

--- ts/tsDemo02/tsDemo02/Vec2.ts
+++ ts/tsDemo02/tsDemo02/Vec2.ts
@@ -1,10 +1,6 @@
 ﻿class Vec2 {
-    x: number;
-    y: number;
 
-    constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
+    constructor(public x: number, public y: number) {
     }
 
     // BEGIN general arithmatic calls
