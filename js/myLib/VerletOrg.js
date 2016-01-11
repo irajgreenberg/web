04b5c1da87a485ac09c0b1f8f7cce7dec7ef0fa5
irajgreenberg <irajgreenberg@gmail.com>
2016-01-11 07:43:58 -0600

    fixes to Verlet Example

--- js/myLib/VerletOrg.js
+++ js/myLib/VerletOrg.js
@@ -1,35 +1,45 @@
 // Verlet Org class
 
-function VerletOrg(pos, vecs, index, rules) {
+function VerletOrg(pos, vecs, indices, rules) {
 	this.pos = pos;
 	this.vecs = vecs;
 	
 	this.nodes = [];
 	this.sticks = [];
-for(var i=0; i<this.vecs.length; i++) {
-	this.nodes[i] = new VerletBall(this.vecs[i]):
-}
+	for(var i=0; i<this.vecs.length; i++) {
+		this.nodes[i] = new VerletNode(this.vecs[i]);
+	}
 
-for(var i=0; i<index.length; i++) {
-	this.sticks[i] = new VerletStick(this.nodes[index[i].elem1], this.nodes[index[i].elem2], .09):
-}
+	for(var i=0; i<indices.length; i++) {
+		//console.log(indices[i].elem1 + ", " + indices[i].elem2);
+		this.sticks[i] = new VerletStick(this.nodes[indices[i].elem1], this.nodes[indices[i].elem2], .02, indices[i].isVisible);
+	}
 
 	//this.spd = new Vec2(.25, .3);
 	//this.pos.add(offset);
 };
 
+VerletOrg.prototype.push = function(nodeID, offset){
+	this.nodes[nodeID].push(offset);
+};
+
+
 VerletOrg.prototype.run = function(){
-	for(var i=0; i<sticks.length; i++) {
-		sticks[i].run();
+	for(var i=0; i<this.nodes.length; i++) {
+		this.nodes[i].verlet();
+	}
+
+	for(var i=0; i<this.sticks.length; i++) {
+		this.sticks[i].constrainLen();
 	}
 };
 
 VerletOrg.prototype.move = function(){
-	this.pos.add(spd);
+	//this.pos.add(spd);
 };
 
 VerletOrg.prototype.display = function(ctx){
-	for(var i=0; i<sticks.length; i++) {
+	for(var i=0; i<this.sticks.length; i++) {
 		this.sticks[i].displayStick(ctx, 8, "blue");
 		this.sticks[i].displayNodes(ctx, 4, "orange");
 	}
--- js/myLib/VerletStick.js
+++ js/myLib/VerletStick.js
@@ -1,12 +1,14 @@
 // Verlet Stick class
 
-function VerletStick(n1, n2, stiffness) {
+function VerletStick(n1, n2, stiffness, isVisible) {
 	this.n1 = n1;
 	this.n2 = n2;
 	this.stiffness = stiffness;
+    this.isVisible = isVisible;
 
 	this.vecOrig = new Vec2(this.n2.pos.x-this.n1.pos.x, this.n2.pos.y-this.n1.pos.y);
-	this.len = Math.sqrt((this.n2.pos.x-this.n1.pos.x)*(this.n2.pos.x-this.n1.pos.x), (this.n2.pos.y-this.n1.pos.y)*(this.n2.pos.y-this.n1.pos.y));
+	this.len = Math.sqrt( (this.n2.pos.x-this.n1.pos.x)*(this.n2.pos.x-this.n1.pos.x) +  
+                        (this.n2.pos.y-this.n1.pos.y)*(this.n2.pos.y-this.n1.pos.y) );
 };
 
 VerletStick.prototype.push = function(node, offset){
@@ -17,16 +19,13 @@ VerletStick.prototype.push = function(node, offset){
     }
 };
 
-VerletStick.prototype.run = function(){
-    this.n1.verlet();
-    this.n2.verlet();
-    this.constrainLen();
-};
-
 VerletStick.prototype.constrainLen = function(){
+   // this.n1.verlet();
+   // this.n2.verlet();
     var delta = new Vec2(this.n2.pos.x-this.n1.pos.x, this.n2.pos.y-this.n1.pos.y);
     var deltaLength = delta.mag();
     var difference = ((deltaLength - this.len) / deltaLength);
+
     this.n1.pos.x += delta.x * (0.5 * this.stiffness * difference);
     this.n1.pos.y += delta.y * (0.5 * this.stiffness * difference);
     this.n2.pos.x -= delta.x * (0.5 * this.stiffness * difference);
@@ -34,12 +33,14 @@ VerletStick.prototype.constrainLen = function(){
   };
 
   VerletStick.prototype.displayStick= function(ctx, wt, col){
-    ctx.lineWidth=wt;
-    ctx.beginPath();
-    ctx.moveTo(this.n1.pos.x, this.n1.pos.y);
-    ctx.lineTo(this.n2.pos.x, this.n2.pos.y);
-    ctx.strokeStyle = col;
-    ctx.stroke();
+    if(this.isVisible){
+        ctx.lineWidth=wt;
+        ctx.beginPath();
+        ctx.moveTo(this.n1.pos.x, this.n1.pos.y);
+        ctx.lineTo(this.n2.pos.x, this.n2.pos.y);
+        ctx.strokeStyle = col;
+        ctx.stroke();
+    }
   };
 
   VerletStick.prototype.displayNodes= function(ctx, rad, col){
